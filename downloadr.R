996ac4667883bc3f9f11929d0eb0b8db56c3bf4a
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2026-08-11 18:23:44 -0400

    restructure and reduce single-use vars and funcs

--- downloadr.R
+++ downloadr.R
@@ -39,16 +39,15 @@ PACKAGES_CSV <- "./packages.csv"
 
 # ensure required packages are installed and loaded
 
-deps <- base::c("readr", "dplyr", "purrr", "ctv", "tools")
-
-checkDep <- function(dep) {
-  if (!require(dep, character.only = TRUE)) {
-    base::install.packages(dep)
-    library(dep, character.only = TRUE)
+base::invisible(base::lapply(
+  base::c("readr", "dplyr", "purrr", "ctv", "tools"),
+  \(dep) {
+    if (!require(dep, character.only = TRUE)) {
+      base::install.packages(dep)
+      library(dep, character.only = TRUE)
+    }
   }
-}
-
-base::invisible(base::lapply(deps, checkDep))
+))
 
 
 ## MAIN
@@ -56,26 +55,24 @@ base::invisible(base::lapply(deps, checkDep))
 # get package list from task views
 
 tv_status <- readr::read_csv(TASKVIEWS_CSV, show_col_types = FALSE)
-tv_list <- ctv::available.views(repos = REPOS)
 
-processTV <- function(tv) {
-  status <- tv_status %>%
-    dplyr::filter(name == tv$name) %>%
-    dplyr::pull(status) %>%
-    dplyr::first()
-  if (status == 1) {
-    tv$packagelist %>%
-      dplyr::filter(core == TRUE) %>%
-      dplyr::pull(name) %>%
-      return()
-  } else if (status == 2) {
-    return(tv$packagelist$name)
-  } else {
-    return(NULL)
-  }
-}
-
-tv_packages <- base::lapply(tv_list, processTV) %>%
+tv_packages <- ctv::available.views(repos = REPOS) %>%
+  base::lapply(\(tv) {
+    status <- tv_status %>%
+      dplyr::filter(name == tv$name) %>%
+      dplyr::pull(status) %>%
+      dplyr::first()
+    if (status == 1) {
+      tv$packagelist %>%
+        dplyr::filter(core == TRUE) %>%
+        dplyr::pull(name) %>%
+        return()
+    } else if (status == 2) {
+      return(tv$packagelist$name)
+    } else {
+      return(NULL)
+    }
+  }) %>%
   purrr::flatten() %>%
   base::unlist() %>%
   base::unique()
@@ -83,14 +80,14 @@ tv_packages <- base::lapply(tv_list, processTV) %>%
 
 # add packages from specified package list if needed
 
-desired_packages <- readr::read_csv(PACKAGES_CSV, show_col_types = FALSE) %>%
+csv_packages <- readr::read_csv(PACKAGES_CSV, show_col_types = FALSE) %>%
   dplyr::filter(status == 1) %>%
-  dplyr::pull(name) %>%
-  base::c(tv_packages) %>%
-  base::unique()
+  dplyr::pull(name)
+
+desired_packages <- base::union(csv_packages, tv_packages)
 
 
-# drop any desired package with dependencies not available via REPOS
+# derive dependencies and determine package availability
 
 repo_packages <- utils::available.packages(repos = REPOS)
 
@@ -103,33 +100,23 @@ package_deps <- tools::package_dependencies(
 
 base_packages <- base::rownames(utils::installed.packages(priority = "base"))
 repo_packages <- base::rownames(repo_packages)
-available_packages <- base::c(repo_packages, base_packages)
+available_packages <- base::union(repo_packages, base_packages)
 
 
-desired_packages <- base::Filter(function(pkg) {
+# drop any desired package with dependencies not available via REPOS
+
+desired_packages <- base::Filter(\(pkg) {
   closure <- base::c(pkg, package_deps[[pkg]])
   missing <- base::setdiff(closure, available_packages)
   if (length(missing) > 0) {
     warning(base::sprintf(
       "dropping '%s': requires unavailable package(s): %s",
-      pkg, paste(missing, collapse = ", ")
+      pkg, base::paste(missing, collapse = ", ")
     ), call. = FALSE)
     return(FALSE)
   }
   TRUE
 }, desired_packages)
-
-
-# get dependencies and add to download list if needed
-
-all_packages <- purrr::flatten(package_deps[desired_packages]) %>%
-  base::c(desired_packages) %>%
-  base::unique()
-
-
-# remove packages included in base R
-
-final_packages <- purrr::discard(all_packages, function(x) x %in% base_packages)
 
 
 # check if download directory exists, create if needed
@@ -138,11 +125,12 @@ if (!base::dir.exists(DESTDIR)) {
   base::dir.create(DESTDIR)
 }
 
-# download required packages and output list of downloaded packages
+# remove base R packages and download final selection
 
-packagelist <- utils::download.packages(final_packages, DESTDIR,
-  repos = REPOS,
-  type = PKGTYPE
-) %>% base::data.frame()
-base::colnames(packagelist) <- c("package", "file")
-readr::write_csv(packagelist, DOWNLOADS_CSV)
+purrr::flatten(package_deps[desired_packages]) %>%
+  base::union(desired_packages) %>%
+  base::setdiff(base_packages) %>%
+  utils::download.packages(DESTDIR, repos = REPOS, type = PKGTYPE) %>%
+  base::data.frame() %>%
+  purrr::set_names(c("package", "file")) %>%
+  readr::write_csv(DOWNLOADS_CSV)
